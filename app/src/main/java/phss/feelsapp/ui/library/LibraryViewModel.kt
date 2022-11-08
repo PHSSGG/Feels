fba103a7c0ae22eaef79e84ab4ae39104cab4cce
PHSSGG <pedrohddgd@gmail.com>
2022-11-08 17:14:14 -0300

    fixup! implemented the playlists system and finished the library fragment

---
+++ app/src/main/java/phss/feelsapp/ui/library/LibraryViewModel.kt
@@ -0,0 +1,38 @@
+package phss.feelsapp.ui.library
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import phss.feelsapp.data.models.Playlist
+import phss.feelsapp.data.repository.PlaylistsRepository
+import phss.feelsapp.data.repository.SongsRepository
+
+class LibraryViewModel(
+    private val playlistsRepository: PlaylistsRepository,
+    private val songsRepository: SongsRepository
+) : ViewModel() {
+
+    fun getPlaylists(): Flow<List<Playlist>> {
+        return playlistsRepository.getAllPlaylists()
+    }
+
+    fun createPlaylist(newPlaylistName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val playlistModel = Playlist(
+                playlistId = 0,
+                playlistName = newPlaylistName
+            )
+            playlistsRepository.createPlaylist(playlistModel)
+        }
+    }
+
+    fun deletePlaylist(playlist: Playlist) {
+        viewModelScope.launch(Dispatchers.IO) {
+            playlistsRepository.deletePlaylist(playlist)
+        }
+    }
+
+}
